c8f4dce71c12df148ed7bd4818008d3d69806fa3
jo-qzy <jo2651933495@gmail.com>
2021-05-08 20:16:36 +0800

    Init project, add cmakelist.

---
+++ src/event.h
@@ -0,0 +1,15 @@
+//
+// Created by BoringWednesday on 2021/5/8.
+//
+
+#ifndef __FAKE_EVENT_H__
+#define __FAKE_EVENT_H__
+
+
+class BaseEvent
+{
+
+};
+
+
+#endif //__FAKE_EVENT_H__
---
+++ test/test_server.cpp
@@ -0,0 +1,8 @@
+//
+// Created by BoringWednesday on 2021/5/8.
+//
+
+int main()
+{
+    return 0;
+}
